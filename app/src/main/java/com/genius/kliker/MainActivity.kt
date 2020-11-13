03097e8164e9668521762e31e30e8fa5972b33cc
geniusserg <danilovsergeydmitrievich@gmail.com.com>
2020-11-13 19:00:40 +0300

    need to fix probability

--- app/src/main/java/com/genius/kliker/MainActivity.kt
+++ app/src/main/java/com/genius/kliker/MainActivity.kt
@@ -1,67 +1,63 @@
 package com.genius.kliker
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AccelerateInterpolator
 import android.widget.Button
+import androidx.core.view.ViewCompat
 
 var active : Boolean = false
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button : Button
-    var x:Float? = 0F
-    var y:Float? = 0F
-    val xmax = 10F
-    val ymax = 10F
-    var step = 0.01F
-    var arg = 1F
+    var button : Button? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
-        x = button.x
-        y = button.y
-        button.setOnClickListener {
-            View.OnClickListener {
-                active = false
-                val intent = Intent(applicationContext, WonActivity::class.java)
-                startActivity(intent)
-            }
+        button?.x = 0F
+        button?.y = 0F
+        button?.setOnClickListener {
+            active = false
+            val intent = Intent(this@MainActivity, WonActivity::class.java)
+            startActivity(intent)
+            button?.clearAnimation()
         }
-        Thread{
-            active = true
-            while (active) {
-                Thread.sleep(1000)
-                runOnUiThread {
-                    x?.let {button.setX(it)}
-                    getYparam()?.let { button.setY(it) }
-                    x?.plus(step)
-                }
-            }
+        Thread {
+            animateManager()
         }.start()
     }
-    fun getYparam() : Float? {
-        if (x == xmax || x == 0F){
-            step = -step
-            button.setBackgroundColor((Color.BLACK..Color.WHITE).random())
+
+    private fun animateManager() {
+        active = true
+        while (active) {
+            runOnUiThread {
+                ViewCompat.animate(findViewById(R.id.button))
+                        .translationX((1000..5000).random() / 10F)
+                        .translationY((1000..8000).random() / 10F)
+                        .setDuration(200)
+            }
+            Thread.sleep(250)
+            button?.setBackgroundColor((Color.BLACK..Color.WHITE).random())
         }
-        if (y == ymax || y == 0F){
-            arg = -arg
-            button.setBackgroundColor((Color.BLACK..Color.WHITE).random())
-        }
-        y = y?.plus(arg*step)
-        print(y)
-        return y
     }
+
+    override fun onResume() {
+        super.onResume()
+        Thread {animateManager()}.start()
+    }
+
 }
 
 class WonActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_won)
-        active = false
     }
 }
